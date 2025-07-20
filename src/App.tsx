018761b10d2504cd503ea68fe5713d8dcab471f3
eman <emanabdelrhman6272@gmail.com>
2025-07-20 20:32:44 +0300

    after abstract data of teacher and student from firebase

--- src/App.tsx
+++ src/App.tsx
@@ -34,16 +34,68 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/ForgetPassword" element={<ForgetPassword />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profileStd" element={<ProtectedRoute allowedRoles={["student"]}><ProfileStudent /></ProtectedRoute>} />
-          <Route path="/profileTeacher" element={<ProtectedRoute allowedRoles={["teacher"]}><ProfileTeacher /></ProtectedRoute>} />
+          <Route
+            path="/profileStd"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <ProfileStudent />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profileTeacher"
+            element={
+              <ProtectedRoute allowedRoles={["teacher"]}>
+                <ProfileTeacher />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profileTeacher/courseDetails/payment" element={<ProtectedRoute allowedRoles={["student"]}><Payment /></ProtectedRoute>} />
-          <Route path="/admin" element={<ProtectedRoute allowedRoles={["admin"]}><AdminDashboard1 /></ProtectedRoute>} />
-          <Route path="/Courses" element={<ProtectedRoute allowedRoles={["admin"]}><Courses /></ProtectedRoute>} />
-          <Route path="/Teachers" element={<ProtectedRoute allowedRoles={["admin"]}><Teachers /></ProtectedRoute>} />
-          <Route path="/Students" element={<ProtectedRoute allowedRoles={["admin"]}><Students /></ProtectedRoute>} />
-          <Route path="/profileTeacher/courseDetails" element={<CourseDetalis />} />
+          <Route
+            path="/profileTeacher/courseDetails/payment"
+            element={
+              <ProtectedRoute allowedRoles={["student"]}>
+                <Payment />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <AdminDashboard1 />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Courses"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <Courses />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Teachers"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <Teachers />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Students"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <Students />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profileTeacher/courseDetails"
+            element={<CourseDetalis />}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </SnackbarProvider>
